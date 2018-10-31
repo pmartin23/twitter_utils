9271603d99ee52aaef71ca99a11eb3a932ee7b9b
pmartin23 <paige.newman@mq.edu.au>
2018-10-31 13:43:28 +1100

    increment version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r",encoding='utf-8') as fh:
 
 setuptools.setup(
     name="twitter_utils",
-    version="0.0.1",
+    version="0.0.2",
     author="Paige Martin",
     author_email="paige.newman@mq.edu.au",
     description="Tools for retrieving data from Twitter",
